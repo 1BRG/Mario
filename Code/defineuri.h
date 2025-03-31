e7a60d87f01928bdb5f4724a751d5ed105742106
1BRG <balaceanurafael@gmail.com>
2025-03-31 21:44:58 +0300

    Super Mario

---
+++ Code/defineuri.h
@@ -0,0 +1,17 @@
+//
+// Created by Gabriel on 3/31/2025.
+//
+
+#ifndef DEFINEURI_H
+#define DEFINEURI_H
+
+#define MarioTexture LoadTexture("../Texture/MarioTest.png")
+#define BrickTexture LoadTexture("../Texture/Bricks.png")
+#define screenWidth 800
+#define screenHeight 600
+#define DefaultSpeed 1
+#define ProcentAlergare 0.05
+#define JumpMax (lastY-100)
+#define JumpMin (lastY-50)
+#define Jump 2
+#endif //DEFINEURI_H
